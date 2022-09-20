0f441617271a7cda73f8032a7f68e4af2d64fc90
Jesse Shawl <jesse@jesse.sh>
2022-09-19 19:17:10 -0500

    add .on() method (#3)
    * test for .on event firings
    
    * clean up tests
    
    * format
    
    * enforce changelog changes
    
    * less specific ref
    
    * quietly
    
    * exit 1
    
    * update changelog
    
    * update changelog
    
    * update docs

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+- add .on(eventName, callback) to dispatch events
+
 ## [0.0.3] - 2022-09-19
 
 ### Added
--- script.js
+++ script.js
@@ -130,8 +130,15 @@ export default function (opts) {
     onChange(e);
   }
 
+  let listeners = {};
+
   function onChange(e) {
-    if (!dragging) return;
+    if (typeof listeners.change === "function") {
+      listeners.change(e, {
+        start: rangeStart.value,
+        end: rangeEnd.value,
+      });
+    }
     const tr = targetRange(e.target);
     const condition = parseInt(rangeEnd.value) <= rangeStart.value;
     if (tr.classList.contains("range-end") && condition) {
@@ -142,4 +149,12 @@ export default function (opts) {
     }
     render();
   }
+
+  function on(name, callback) {
+    listeners[name] = callback;
+  }
+
+  return {
+    on,
+  };
 }
